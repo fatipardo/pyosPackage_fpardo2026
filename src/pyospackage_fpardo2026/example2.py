327b21474e182164798352f7eb43c68e9875a853
Fatima Pardo <31224719+fatipardo@users.noreply.github.com>
2026-04-17 22:42:01 -0700

    new function and module

---
+++ src/pyospackage_fpardo2026/example2.py
@@ -0,0 +1,42 @@
+"""
+A module to learn how to make a package
+so it contains some basic functions
+
+"""
+
+def hello_world(language):
+    """
+    Prints "hello world" in the language specified.
+    Options are spanish, english, portuguese.
+
+    Parameters
+    ----------
+    language: string
+        spanish, english, portuguese
+    
+
+    Returns
+    -------
+    str
+        string "hello wold" in language specified by user
+
+    Examples
+    --------
+    >>> hello_world(english)
+    hello world
+    >>> add_numbers(spanish)
+    hola mundo
+    """
+
+    hello=""
+    
+    if language == "spanish":
+        hello="hola mundo"
+    elif language== "english":
+        hello="hello world"
+    elif language == "portuguese":
+        hello="oi mundo"
+    else:
+        hello="I do not understand, try another language"
+    
+    print(hello)
